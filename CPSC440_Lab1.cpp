0c17bc5a0ad2c0d82d737c0c207e1deabc84a97c
adantuon <adantuon@mail.umw.edu>
2025-05-20 13:47:00 -0400

    Remembering to do some in-progress commits to show my work

--- CPSC440_Lab1.cpp
+++ CPSC440_Lab1.cpp
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void makeTurtle();
+
 int main()
 {
     ALLEGRO_DISPLAY *display = NULL;
@@ -21,15 +23,56 @@ int main()
         return(-1);
     }
 
+    al_init_primitives_addon();
+
     al_clear_to_color(al_map_rgb(0, 0, 0));
+
+    makeTurtle();
 
     al_flip_display();
 
-    al_rest(10.0);
+    al_rest(5.0);
     
     al_destroy_display(display);
 
     return 0;
+}
+
+void makeTurtle() {
+    //Color Palette
+    ALLEGRO_COLOR light_green = al_map_rgb(0, 100, 0);
+    ALLEGRO_COLOR dark_green = al_map_rgb(0, 50, 0);
+    ALLEGRO_COLOR white = al_map_rgb(255, 255, 255);
+    ALLEGRO_COLOR black = al_map_rgb(0, 0, 0);
+
+    //Make Head
+    al_draw_filled_circle(580, 300, 75, light_green);
+    al_draw_filled_ellipse(590, 350, 20, 15, white);
+    al_draw_filled_ellipse(590, 250, 20, 15, white);
+
+    //Make Tail
+    al_draw_filled_triangle(175, 320, 175, 280, 115, 300, light_green);
+
+    //Make Legs
+    al_draw_filled_circle(200, 200, 50, light_green);
+    al_draw_filled_circle(200, 400, 50, light_green);
+    al_draw_filled_circle(500, 200, 50, light_green);
+    al_draw_filled_circle(500, 400, 50, light_green);
+
+    //Make Shell
+    al_draw_filled_ellipse(350, 300, 200, 150, light_green);
+
+    //Make Shell Lines
+    al_draw_ellipse(350, 300, 200, 150, dark_green, 8);
+    al_draw_line(350, 300, 200, 200, dark_green, 8);
+    al_draw_line(350, 300, 200, 400, dark_green, 8);
+    al_draw_line(350, 300, 500, 200, dark_green, 8);
+    al_draw_line(350, 300, 500, 400, dark_green, 8);
+    al_draw_line(150, 300, 550, 300, dark_green, 8);
+    al_draw_line(350, 150, 350, 450, dark_green, 8);
+
+    al_draw_filled_ellipse(350, 300, 80, 60, light_green);
+    al_draw_ellipse(350, 300, 80, 60, dark_green, 8);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
